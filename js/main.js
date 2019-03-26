1a1d0587ea8f3aa64a9324e89bc1a4a047dcaa0b
MRybachok <rybachokmykhailo@gmail.com>
2019-03-26 19:20:17 +0200

    added new images

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+$(".nav-bar__hamburger").on("click", function(){
+    $(".nav-bar__hamburger").toggleClass("nav-bar__hamburger--active");
+    $(".nav-bar__menu").toggleClass("nav-bar__menu--active");
+
+});
+
+// nav-bar__menu--active
+// nav-bar__hamburger--active
--- main.js
+++
@@ -1,9 +0,0 @@
-
-$(".nav-bar__hamburger").on("click", function(){
-    $(".nav-bar__hamburger").toggleClass("nav-bar__hamburger--active");
-    $(".nav-bar__menu").toggleClass("nav-bar__menu--active");
-
-});
-
-// nav-bar__menu--active
-// nav-bar__hamburger--active
