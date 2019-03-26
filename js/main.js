e40271d9f59fe84c326b872076c418383dbd7013
MRybachok <rybachokmykhailo@gmail.com>
2019-03-26 20:51:46 +0200

    new modal window

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-
+// nav-bar
 $(".nav-bar__hamburger").on("click", function(){
     $(".nav-bar__hamburger").toggleClass("nav-bar__hamburger--active");
     $(".nav-bar__menu").toggleClass("nav-bar__menu--active");
 
 });
 
-// nav-bar__menu--active
-// nav-bar__hamburger--active
+// modal window
+$(".modal-btn").on("click", function(){
+    $("body").toggleClass("modal-open");
+    $(".modal-window").toggleClass("modal-window--active");
+
+});
+
+$(".modal-window__close").on("click", function(){
+    $("body").removeClass("modal-open");
+    $(".modal-window").removeClass("modal-window--active");
+});
+
